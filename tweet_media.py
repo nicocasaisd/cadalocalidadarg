5b55364056ee717c61e5cc7c22243b2ac8eda439
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-07-25 19:38:38 -0300

    prueba media tweet

--- tweet_media.py
+++ tweet_media.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     api = create_api()
     filename = "composite_images/localidad.png"
     status = "Prueba"
-    api.update_with_media(filename, status)
+    api.media_upload(filename)
