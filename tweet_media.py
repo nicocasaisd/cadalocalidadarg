d786013a2566696ca1458c1f5aba9f9fb6d10649
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-07-28 23:48:17 -0300

    borro archivos sin uso

--- tweet_media.py
+++
@@ -1,8 +0,0 @@
-import os
-from config import create_api
-
-if __name__ == "__main__":
-    api = create_api()
-    filename = "composite_images/localidad.png"
-    status = "Prueba"
-    api.update_status_with_media(status, filename)
